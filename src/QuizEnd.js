93303a48529712b091d6d63635e501517d3e2b4b
mnhoque <naz05haq@gmail.com>
2021-02-16 09:20:04 +0100

    module7

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,19 +1,17 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-class QuizEnd extends Component{
-    handleResetClick(){
-
-    }
-    render(){
-        return(
-            <div>
-                <p>
-                    Thanks for playing!
-                </p>
-                <a href ='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
-            </div>
-        )
-    }
-
+class QuizEnd extends Component {
+  handleResetClick() {
+    this.props.resetClickHandler()
+  }
+  render() {
+    return (
+      <div>
+        <p>Thanks for playing!</p>
+        <a href='' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
+      </div>      
+    )
+  }
 }
-export default QuizEnd
+
+export default QuizEnd 
